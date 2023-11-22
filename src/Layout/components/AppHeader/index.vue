6c1f1f2caa79242afe818d3c68e0a33c88e5f383
lovewei0 <xdw666888@qq.com>
2023-11-22 10:30:18 +0800

    feat(avatar): add

--- src/Layout/components/AppHeader/index.vue
+++ src/Layout/components/AppHeader/index.vue
@@ -12,15 +12,43 @@
     <Breadcrumb class="breadcrumb-container" />
     <!-- 右边菜单 -->
     <div class="right-menu">
+      <!-- 全屏 -->
       <ScreenFull class="right-menu-item hover-effect" />
+      <!-- 头像 -->
+      <el-dropdown class="avatar-container" trigger="click">
+        <div class="avatar-wrapper">
+          <el-avatar :size="40" shape="square"></el-avatar>
+        </div>
+        <template #dropdown>
+          <el-dropdown-menu class="user-dropdown">
+            <RouterLink to="/">
+              <el-dropdown-item>首页</el-dropdown-item>
+            </RouterLink>
+            <a target="_black" href="">
+              <el-dropdown-item>个人中心</el-dropdown-item>
+            </a>
+            <el-dropdown-item divided @click="logoOut">
+              退出登录
+            </el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
     </div>
   </div>
 </template>
 
 <script setup>
+// components
 import Hamburger from './components/Hamburger.vue'
 import Breadcrumb from './components/Breadcrumb.vue'
 import ScreenFull from './components/ScreenFull.vue'
+// vue-router
+import { RouterLink, useRouter } from 'vue-router'
+const router = useRouter()
+const logoOut = () => {
+  console.log(123)
+  router.push('/login')
+}
 </script>
 
 <style scoped lang="scss">
@@ -54,6 +82,16 @@ import ScreenFull from './components/ScreenFull.vue'
       line-height: 46px;
       &.hover-effect {
         cursor: pointer;
+      }
+    }
+    ::v-deep(.avatar-container) {
+      cursor: pointer;
+      .avatar-wrapper {
+        margin-top: 5px;
+        position: relative;
+        .el-avatar {
+          margin-right: 12px;
+        }
       }
     }
   }
